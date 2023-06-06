c974e513efad1907f85ede644b1646a4247e2283
André Soares Gomes <andrsoares953@yahoo.com>
2023-06-06 09:15:47 -0300

    feat: Added two native examples in c#

---
+++ IObserverIObservable/Program.cs
@@ -0,0 +1,90 @@
+﻿class Program : IObserver<float>
+{
+	public Market market { get; set; }
+	private IDisposable subscription;
+	public Program()
+	{
+		market = new Market();
+
+		market.Subscribe(this);
+
+		//Add prices
+		market.AddPrice(220);
+		market.AddPrice(380);
+		market.AddPrice(197);
+
+		subscription = market.Subscribe(this);
+
+		this.Unsubscribe();
+	}
+
+	static void Main(string[] args)
+	{
+		Program program = new Program();
+	}
+
+	public void OnCompleted()
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnError(Exception error)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnNext(float value)
+	{
+		Console.WriteLine($"Added a new price in market: {value}");
+	}
+
+	public void Unsubscribe()
+	{
+		subscription.Dispose();
+	}
+}
+
+
+class Market : IObservable<float>
+{
+	public List<float> Prices { get; private set; }
+	private List<IObserver<float>> Observers { get; set; } 
+
+	public Market()
+	{
+		Prices = new List<float>();
+		Observers = new List<IObserver<float>>();
+	}
+
+	public void AddPrice(float price)
+	{
+		Prices.Add(price);
+
+		foreach (var observer in Observers)
+		{
+			observer.OnNext(price);
+		}
+	}
+
+	public IDisposable Subscribe(IObserver<float> observer)
+	{
+		Observers.Add(observer);
+
+		return new Subscription(observer);
+	}
+}
+
+class Subscription : IDisposable
+{
+	private IObserver<float> observer;
+
+	public Subscription(IObserver<float> observer)
+	{
+		this.observer = observer;
+	}
+
+	public void Dispose()
+	{
+		observer = null;
+	}
+}
---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+
+//This is a example using raw c# that is possible to make reactivity system
+
+//observer
+
+var market = new Market();
+market.Prices.ListChanged += (sender, eventArgs) =>
+{
+	if (eventArgs.ListChangedType == ListChangedType.ItemAdded)
+	{
+		var lastPriceAdded = market.Prices[eventArgs.NewIndex];
+		Console.WriteLine($"Last price added: {lastPriceAdded}");
+	}
+
+	if (eventArgs.ListChangedType == ListChangedType.ItemChanged)
+	{
+		var lastPriceChanged = market.Prices[eventArgs.NewIndex];
+		Console.WriteLine($"Last price changed: {lastPriceChanged}");
+	}
+
+	if (eventArgs.ListChangedType == ListChangedType.ItemDeleted)
+	{
+		var lastPriceDeleted = eventArgs.OldIndex;
+		Console.WriteLine($"Last price deleted: {lastPriceDeleted}");
+	}
+
+	if (eventArgs.ListChangedType == ListChangedType.Reset)
+	{
+		Console.WriteLine("Prices reset");
+	}
+};
+
+market.Prices.Add(20);
+
+class Market //observable
+{
+	public BindingList<float> Prices { get; set; } = new();
+}
