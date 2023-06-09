f436f70e9746c49bc98bc9a0fa09e75b52018a2b
André Soares Gomes <andrsoares953@yahoo.com>
2023-06-08 23:31:39 -0300

    Feat: Added project with sequence filtering example

---
+++ SequenceFilters/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Reactive.Linq;
+using SequenceFilters.Classes;
+
+var value = Observable.Range(-10, 20);
+
+value.SkipWhile(s => s < 0)
+	.TakeWhile(s => s <= 6)
+	.Inspect(nameof(value));
