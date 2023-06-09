80dcbc68f7fdcc7ae358b4e16b6ed702c468d4ae
André Soares Gomes <andrsoares953@yahoo.com>
2023-06-09 00:22:02 -0300

    Feat: finished all examples

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive.Subjects;
+
+var subject = new Subject<int>();
+
+subject.Subscribe(
+	onNext: value => Console.WriteLine($"Received value: {value}"),
+	onError: ex => Console.WriteLine($"Error: {ex.Message}"),
+	onCompleted: () => Console.WriteLine("Completed")
+);
+
+subject.OnNext(1);
+subject.OnNext(2);
+
+try
+{
+	throw new Exception("Something went wrong");
+}
+catch (Exception ex)
+{
+	subject.OnError(ex);
+}
+
+subject.OnNext(3);
+subject.OnCompleted();
+
+Console.ReadKey();
