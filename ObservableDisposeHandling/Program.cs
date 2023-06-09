082233f583a1f15567b707443f01ebb053fd1395
André Soares Gomes <andrsoares953@yahoo.com>
2023-06-08 22:46:51 -0300

    Feat: Dispose handling for observer

---
+++ ObservableDisposeHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Timers;
+using ObservableDisposeHandling.Classes;
+using Timer = System.Timers.Timer;
+
+const int ONE_SECOND = 1000;
+
+var obs = Observable.Create<string>(o =>
+{
+	var timer = new Timer(ONE_SECOND);
+
+	timer.Elapsed += (sender, args) =>
+	{
+		o.OnNext($"Tick {args.SignalTime}");
+	};
+
+	timer.Elapsed += TimerOnElapsed;
+	timer.Start();
+
+	return () =>
+	{
+		o.OnCompleted();
+		timer.Elapsed -= TimerOnElapsed;
+		timer.Dispose();
+	};
+});
+
+var sub = obs.Inspect(nameof(obs));
+Console.ReadLine();
+
+sub.Dispose();
+Console.ReadLine();
+
+static void TimerOnElapsed(object sender, ElapsedEventArgs args)
+{
+	Console.WriteLine($"Tock {args.SignalTime}");
+}
