78c4cb3d41d48aba8a5e41845a382219048b2938
André Soares Gomes <andrsoares953@yahoo.com>
2023-06-08 22:35:01 -0300

    Feat: added simple factory methods

---
+++ FactoryMethods/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Reactive.Linq;
+using FactoryMethods.Classes;
+
+var replayInt = Observable.Return(32);
+replayInt.Inspect(nameof(replayInt));
+
+var empty = Observable.Empty<int>();
+empty.Inspect(nameof(empty));
+
+var error = Observable.Throw<int>(new Exception("Error"));
+error.Inspect(nameof(error));
